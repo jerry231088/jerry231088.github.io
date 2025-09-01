19dda6cf188aa959eeef1c2fe9448310b3759448
jerry231088 <59258661+jerry231088@users.noreply.github.com>
2025-09-01 13:56:36 +0530

    included design and cost category

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -123,7 +123,7 @@ const Portfolio: React.FC = () => {
     },
     {
       category: "AWS Cloud",
-      skills: [ "Lambda", "Batch". "ECS", "Fargate", "API Gateway", "Glue", "SNS", "SQS", "Kinesis", "MSK/Kafka", "DynamoDB", "Neptune", "S3", "LakeFormation", "Athena", "Redshift", "IAM", "Secrets Manager", "SSM Parameters Store", "SES", "Bedrock" ]
+      skills: [ "Lambda", "Batch". "ECS", "Fargate", "API Gateway", "Glue", "SNS", "SQS", "Kinesis", "MSK/Kafka", "DynamoDB", "Neptune", "S3", "Lake Formation", "Athena", "Redshift", "IAM", "Secrets Manager", "SSM Parameters Store", "SES", "ECR", "Bedrock" ]
     },
     {
       category: "Data Engineering",
@@ -148,6 +148,10 @@ const Portfolio: React.FC = () => {
     {
       category: "Visualization & BI",
       skills: [ "Power BI", "Athena", "QuickSight" ]
+    },
+    {
+      category: "Design & Cost"
+      skills: [ "app.diagrams.net", "AWS Pricing Calculator", "Cost Explorer", "Budgets", "Service Quotas"]
     }
   ];
 
