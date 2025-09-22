b939bd5682e355c9bccbaa4af1339aec0f89107e
mercy <jerry231088@gmail.com>
2025-09-22 17:55:00 +0530

    loading cover letter

--- src/components/CoverLetter.tsx
+++ src/components/CoverLetter.tsx
@@ -136,7 +136,7 @@
          >
              {({ loading }) =>
                  loading ? (
-                     'Generating...'
+                     'Loading...'
                  ) : (
                      <>
                          <svg className="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
